69bea569fe18939b0bb453257984a69ba296d6a4
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2023-06-09 11:06:41 -0700

    Navigation bar edits

---
+++ src/pages/acc.tsx
@@ -0,0 +1,8 @@
+// pages/acc.tsx
+import React from 'react';
+
+const AccPage: React.FC = () => {
+  return <h1>Acc Page</h1>;
+};
+
+export default AccPage;
---
+++ src/pages/login.tsx
@@ -0,0 +1,45 @@
+// pages/login.tsx
+import React, { useState } from 'react';
+import Nav from '../components/Nav';
+
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Perform login logic here
+  };
+
+  return (
+    <div>    
+    <Nav></Nav>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+        <h2>New User?</h2>
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
